8aeeee0831af76f8ab2b06041916e66cc5d5534a
chrisjoonlee <christopherjoon@gmail.com>
2025-08-06 17:32:40 -0300

    .

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -1,6 +1,6 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
-const Button = React.forwardRef(({ text, onClick, disabled = false, bgColor = "bg-primary", textColor = "text-white", hoverColor = "bg-primary-light", onKeyDown, }, ref) => {
+const Button = React.forwardRef(({ text, onClick, disabled = false, bgColor = "bg-blue-500", textColor = "text-white", hoverColor = "bg-blue-600", onKeyDown, }, ref) => {
     return (_jsx("button", { ref: ref, tabIndex: 0, className: `py-2 px-6 rounded-sm ${bgColor} ${textColor} disabled:opacity-50 transition
                 hover:${hoverColor} ${disabled ? "" : "cursor-pointer"}
             `, onClick: onClick, disabled: disabled, onKeyDown: onKeyDown, children: text }));
